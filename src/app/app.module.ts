6379ebb2cbdc29842015df090557f29a4c6d8089
ofmunozm <of.munoz@uniandes.edu.co>
2024-11-01 23:05:32 -0500

    creacion del modulo plantas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlantasModule } from './plantas/plantas.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PlantasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
