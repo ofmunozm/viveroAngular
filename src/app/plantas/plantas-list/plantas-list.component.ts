b82ddb9e58d210e0a5f4ee0824da34835af60af8
ofmunozm <of.munoz@uniandes.edu.co>
2024-11-02 00:30:46 -0500

    se agrega linea de totales al final e imagen de presentacion

--- src/app/plantas/plantas-list/plantas-list.component.ts
+++ src/app/plantas/plantas-list/plantas-list.component.ts
@@ -10,13 +10,23 @@ import { PlantasService } from '../plantas.service';
 export class PlantasListComponent implements OnInit {
 
   plantas: Array<Planta> = [];
+  totalInterior: number = 0;
+  totalExterior: number = 0;
+
   constructor(private plantasService: PlantasService) { }
 
   getPlantas() {
     this.plantasService.getPlantas().subscribe(plantas => {
       this.plantas = plantas;
+      this.calcularTotales();
     });
   }
+
+  calcularTotales() {
+    this.totalInterior = this.plantas.filter(planta => planta.tipo === 'Interior').length;
+    this.totalExterior = this.plantas.filter(planta => planta.tipo === 'Exterior').length;
+  }
+
   ngOnInit() {
     this.getPlantas();
   }
